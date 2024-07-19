a5748773ecb0b32ff853525299da202358ac6c99
AgustinRagessi <gragess561@alumnos.iua.edu.ar>
2024-07-19 12:30:41 -0300

    register anda, seguir testeando tokens

--- controller/register.controller.js
+++ controller/register.controller.js
@@ -15,6 +15,7 @@ const accountCreation = async (req, res) =>{
             return res.status(401).json({message: "El email " + req.body.email + " ya existe."});
         }else if (createdResponse){
             return res.status(200).json({message: "Se creo el nuevo usuario: " + req.body.username +"."});
+            
         }
     } catch (error) {
         res.status(500).json({message: "Error al crear el usuario"});
